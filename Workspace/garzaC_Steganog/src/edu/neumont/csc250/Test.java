8a8e88aa602dc7e6e4643015b5dd5ff1983023fb
clairegarza2012 <badG_22@yahoo.com>
2014-07-10 09:19:01 -0600

    test

--- Workspace/garzaC_Steganog/src/edu/neumont/csc250/Test.java
+++ Workspace/garzaC_Steganog/src/edu/neumont/csc250/Test.java
@@ -1,37 +1,23 @@
 package edu.neumont.csc250;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
-import javax.swing.Timer;
 import edu.neumont.ui.Picture;
 
 public class Test {
-
-	static double count = 0;
 	
 	public static void main(String[] args) {
 
 		Picture pic = new Picture("jcMessage.png");
 
 		Steganog nog = new Steganog();
-
-		Timer timer = new Timer(1, new TimerListener());
-		timer.start();
 		
-//		Picture picWithMessage = null;
-//
-//		String myMessage = "";
-//		for (int i = 0; i < 10; i++){
-//			myMessage += "a";
-//		}
-//		myMessage += '/';
-//
-//		try {
-//			picWithMessage = nog.embedIntoImage(pic, myMessage);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
+		Picture picWithMessage = null;
+
+		try {
+			picWithMessage = nog.embedIntoImage(pic, "claire/");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 		//picWithMessage.save("newPic.png");
 		Picture pic2 = new Picture("newPic.png");
@@ -39,26 +25,12 @@ public class Test {
 		String message = "";
 
 		try {
-			message = nog.retreiveFromImage(pic);
+			message = nog.retreiveFromImage(picWithMessage);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
 		System.out.println(message);
-		System.out.println(count/1000);
-		
-		timer.stop();
 	}
-
-	private static class TimerListener implements ActionListener{
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			count++;
-		}
-		
-		
-	}
-
 	
 }
