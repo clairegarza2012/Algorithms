3801aabd600d4f4954ebaeeca11473a884f9c51c
clairegarza2012 <badG_22@yahoo.com>
2014-08-25 00:06:54 -0600

    meta data

---
+++ Workspace/.metadata/.plugins/org.eclipse.core.resources/.history/be/102d3aa5d12b0014173f8ae84879bb54
@@ -0,0 +1,11 @@
+package edu.neumont.csc252;
+
+import java.util.List;
+
+public class DfsGraphTraversal {
+
+	public List<List<Integer>> traverse(Graph g){
+
+		return null;
+	}
+}
---
+++ Workspace/garzaC_Boxed/src/edu/neumont/csc252/test/Test.java
@@ -0,0 +1,38 @@
+package edu.neumont.csc252.test;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import edu.neumont.csc252.BfsGraphTraversal;
+import edu.neumont.csc252.Graph;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+
+		Graph g = new Graph(5);
+		g.addEdge(0, 1, 1);
+		g.addEdge(1, 2, 1);
+		g.addEdge(1, 3, 1);
+		g.addEdge(2, 4, 1);
+		g.addEdge(2, 3, 1);
+		g.addEdge(3, 4, 1);
+
+		BfsGraphTraversal bfs = new BfsGraphTraversal();
+		List<List<Integer>> list = bfs.traverse(g);
+
+		int count = 0;
+		for (List<Integer> l : list){
+			System.out.print(count++ + " ==> ");
+			for (Integer i : l){
+				System.out.print(i + ", ");
+			}
+			System.out.println();
+		}
+
+		System.out.println("\nEdge#: " + g.ecount());
+	}
+
+}
